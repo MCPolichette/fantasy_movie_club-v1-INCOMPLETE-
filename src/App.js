e375c4b723a260944885aa90324ad567d41f6c8d
MCPolichette <mark@polichette.com>
2019-03-04 11:52:09 -0700

    .gitignoretest

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import key from "./keys.json";
+
+var omdbAPIkey = key.OMDB_API_KEY;
 
 class App extends Component {
+
+
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>{omdbAPIkey}</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+
           <a
             className="App-link"
             href="https://reactjs.org"
