442a652e8c7575132ea31781fc45364b51a41e3c
MCPolichette <mark@polichette.com>
2019-03-13 09:06:29 -0600

    run API coded.  waiting for data from the movie draft

--- src/App.js
+++ src/App.js
@@ -12,55 +12,76 @@ import { Chart } from "react-charts";
 const OMDB_Api = key.OMDB_API.key;
 
 
-
-
-
 // movie IDs below for testing:
 // const blackPantherId = "tt1825683";
 const captainMarvelId = "tt4154664";
 
-// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=e06fa2f6
+// RAW DATA from the draft
+const teams = [
+  {
+    name: "Petunia",
+    movies: [
+      "tt4154664" //Captain Marvel
+    ]
+  }
+]
 function loadMovie(id) {
-
   let queryURL = ("http://www.omdbapi.com/?i=" + captainMarvelId + "&apikey=" + OMDB_Api)
   // let queryURL = ("http://www.omdbapi.com/?t=" + "Lego movie" + "&apikey=" + OMDB_Api)
   axios.get(queryURL,
   ).then(function (response) {
     let results = response.data
-    console.log(results)
+    return (results)
   })
 };
 
-
 class App extends Component {
-state = {
-  movies: movieIds.data,
-  teams: [],
-  graphData:[],
-}
-// a shuffling function for movies.
-shuffleMovies(){
-  let graphData=[];
-  let movies =[].concat(this.state.movies)
-  for (let i = movies.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [movies[i], movies[j]] = [movies[j], movies[i]];
-    let x={title: movies[i].title, data:[[i, movies[i].boxOffice]]}
-    graphData.push(x)
-  } 
-  console.log(this.state.graphData)
-  this.setState({movies, graphData})
-}
-componentDidMount(){
-  this.shuffleMovies();
-  console.log(this.state.movies)
- 
-  // for Each Team, Run API for Each Movie On their list, and push data to an array for that team.
-  // 
+  state = {
+    movies: movieIds.data,
+    graphData: [],
+    movieArray: [],
+    teamArray: []
+  }
+  // a shuffling function for movies.
+  shuffleMovies() {
+    let graphData = [];
+    let movies = [].concat(this.state.movies)
+    for (let i = movies.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [movies[i], movies[j]] = [movies[j], movies[i]];
+      let x = { title: movies[i].title, data: [[i, movies[i].boxOffice]] }
+      graphData.push(x)
+    }
+    console.log(this.state.graphData)
+    this.setState({ movies, graphData })
+  }
 
-  // for each movie in the array, build a dataset for the graph.
-  
-}
+  RunAPI(teams) {
+    // Define the Arrays 
+    let movieArray = [];
+    let teamArray = [];
+    for (let i = 0; i < teams.length; i++) {
+      let thisTeam = {
+        name: teams.name,
+        movies: []
+      }
+      for (let j = 0; j < teams[i].movies.length; j++) {
+        let thisMovie = teams[i].movies[j];
+        let movieObject = loadMovie(thisMovie);
+        thisTeam.movies.push(movieObject)
+        movieArray.push({
+          title: movieObject.title,
+          boxOffice: movieObject
+        });
+      };
+      teamArray.push(thisMovie)
+      this.setState({ movieArray, teamArray })
+    }
+  }
+  componentDidMount() {
+    this.shuffleMovies();
+    console.log(this.state.movies)
+  }
 
   render() {
 
@@ -74,26 +95,26 @@ componentDidMount(){
           {/* <MovieGraph
           data= {this.state.graphData} /> */}
 
-<div
-    style={{
-      width: "400px",
-      height: "300px"
-    }}
-  >
-    <Chart
-      data={this.state.graphData}
-      
-      dark
-      primaryCursor
-      secondaryCursor
-      tooltip
-      series={{type:'bar'}}
-      axes={[
-        { primary: true, type: "linear", position: "bottom" },
-        { type: "linear", position: "left" }
-      ]}
-    />
-  </div>
+          <div
+            style={{
+              width: "400px",
+              height: "300px"
+            }}
+          >
+            <Chart
+              data={this.state.graphData}
+
+              dark
+              primaryCursor
+              secondaryCursor
+              tooltip
+              series={{ type: 'bar' }}
+              axes={[
+                { primary: true, type: "linear", position: "bottom" },
+                { type: "linear", position: "left" }
+              ]}
+            />
+          </div>
 
           <button
             id="testButton"
